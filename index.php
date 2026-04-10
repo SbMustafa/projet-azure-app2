2112a784802740343173fc0e485399b7ab062ba8
SbMustafa <167207682+SbMustafa@users.noreply.github.com>
2026-04-10 14:50:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php
- 
 $password = getenv('DB_PASSWORD'); 
-
-$serverName = "srv-azure-mus.database.windows.net"; 
+$serverName = "srv-azure-mus.database.windows.net";
 $database = "db-app-prod";
-$username = "user_admin"; 
+$username = "user_admin";
 
 echo "<h1>Test de déploiement Azure - Mustapha Amine</h1>";
 
 try {
-    
     $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     echo "<h2 style='color:green;'>✅ TEST RÉUSSI : Connexion SQL établie !</h2>";
-    echo "<p>La communication entre l'App Service et la base SQL est fluide.</p>"; [cite: 16]
+    echo "<p>La communication entre l'App Service et la base SQL est fluide.</p>";
 } catch (PDOException $e) {
     echo "<h2 style='color:red;'>❌ Échec de connexion</h2>";
     echo "Détails de l'erreur : " . $e->getMessage();
